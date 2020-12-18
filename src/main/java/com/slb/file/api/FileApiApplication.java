dc60a9fbbf495296aa47876cc620720def065eca
Samir <=>
2020-12-18 23:47:28 +0100

    deploy - need to reconfigure /root

--- src/main/java/com/slb/file/api/FileApiApplication.java
+++ src/main/java/com/slb/file/api/FileApiApplication.java
@@ -2,12 +2,18 @@ package com.slb.file.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
- public class FileApiApplication {
+ public class FileApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FileApiApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FileApiApplication.class);
+	}
 }
